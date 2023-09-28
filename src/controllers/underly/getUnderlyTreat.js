b0571b493b92dedca3be216dabdeb5effc8c40d6
Ohm0025 <toyohm0025@gmail.com>
2023-09-28 20:51:39 +0700

    draft udApi

--- src/controllers/underly/getUnderlyTreat.js
+++ src/controllers/underly/getUnderlyTreat.js
@@ -1,4 +1,10 @@
-const { CaseOrder, Diagnosis } = require("../../models");
+const {
+  CaseOrder,
+  Diagnosis,
+  Advice,
+  FollowUp,
+  Treatment,
+} = require("../../models");
 
 exports.getUnderlyTreat = async (req, res, next) => {
   try {
@@ -6,7 +12,41 @@ exports.getUnderlyTreat = async (req, res, next) => {
 
     // const listCaseID = await CaseOrder.findAll({ where: { patientId } , include:[{model:}] });
 
-    res.status(201).json({ message: "ejgoj0j" });
+    const allCase = await CaseOrder.findAll({
+      where: { patientId, status: "finish" },
+      attributes: ["id", "updatedAt", "doctorId"],
+      include: [
+        { model: Advice, attributes: ["detail"] },
+        {
+          model: FollowUp,
+          attributes: ["fuHos", "fuOPD", "fuDetail", "fuDate"],
+        },
+        {
+          model: Treatment,
+          attributes: ["txList"],
+        },
+      ],
+      order: [["updatedAt", "DESC"]],
+    });
+
+    const sendingArr = allCase.map((item) => {
+      return {
+        caseId: item.id,
+        caseDate: item.updatedAt,
+        caseDoctor: item.doctorId,
+        caseDetail: item.Advice.detail,
+        caseFollow: item.FollowUp, //{fuHos,fuDate,fuStatus,fuDetail}
+        caseTreatment: Object.entries(
+          JSON.parse(item.Treatment.txList) || {}
+        ).filter((item2) => item2[0] === underlyTitle),
+      };
+    });
+
+    res.status(201).json({
+      sendingArr: [
+        ...sendingArr.filter((item) => item.caseTreatment.length > 0),
+      ],
+    });
   } catch (err) {
     next(err);
   }
