a647231e7fb03661b50d1137795cc3b4e5460f44
Ohm0025 <toyohm0025@gmail.com>
2023-11-22 15:17:36 +0700

    config app

--- src/app.js
+++ src/app.js
@@ -75,7 +75,7 @@ io.on("connection", (socket) => {
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-console.log(waitingPatient);
+// console.log(waitingPatient);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
